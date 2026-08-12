b3f8352fb528de64476f91e841a7ca6c3af4221f
Vamshi <vamshivamsharaj123@gmail.com>
2026-08-12 17:23:34 +0530

    refactor(db): migrate task persistence to Prisma

--- src/lib/db/prisma.ts
+++ src/lib/db/prisma.ts
@@ -1,7 +1,6 @@
-import { PrismaClient } from "@/generated/prisma";
+import { PrismaClient } from "@prisma/client";
 
 declare global {
-  // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
