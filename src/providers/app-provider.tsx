e68c4f92dc25fcc5b473e66e4eda286b6ac36e3f
Vamshi <vamshivamsharaj123@gmail.com>
2026-08-13 21:27:19 +0530

    feat(app): add query and undo providers

--- src/providers/app-provider.tsx
+++ src/providers/app-provider.tsx
@@ -1,15 +1,21 @@
 "use client";
 
-import { TooltipProvider } from "@radix-ui/react-tooltip";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import { CommandPaletteProvider } from "@/contexts/command-palette-context";
+import { UndoProvider } from "@/contexts/undo-context";
+import { QueryProvider } from "@/providers/query-provider";
 import { ThemeProvider } from "@/providers/theme-provider";
 
 export function AppProvider({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-      <TooltipProvider delayDuration={200}>
-        <CommandPaletteProvider>{children}</CommandPaletteProvider>
-      </TooltipProvider>
+      <QueryProvider>
+        <TooltipProvider delayDuration={200}>
+          <CommandPaletteProvider>
+            <UndoProvider>{children}</UndoProvider>
+          </CommandPaletteProvider>
+        </TooltipProvider>
+      </QueryProvider>
     </ThemeProvider>
   );
 }
